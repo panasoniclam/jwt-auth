cf746028b763a52fced6d2b526c38d893f32fb51
NguyenNgocLam <lamnn8@fpt.com.vn>
2020-02-16 12:53:20 +0700

    updapdat

--- test.js
+++ test.js
@@ -1,8 +1,13 @@
+const express = require('express')
+const app = express.Router()
+const jwt = require('json-web-token')
+const server = require('http').Server(app)
+app.post('/',(req,res,next)=>{
+    res.status(200).send({
+        message:"jddjdjdj"
+    })
+})
+server.listen(4000,()=>console.log('server running is port 4000'))
 
-fetch('https://api.randomuser.me')
-.then(response=>{
-    return response.json()
-})
-.then(myJson=>{
-    console.log(JSON.stringify(myJson))
-})
+
+module.exports =app
